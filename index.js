65d1616a86385a0e575ca1ee1afc3a290fcd9ca5
Sung Hyun Hong (Jenny) <57204918+Jenny9197@users.noreply.github.com>
2021-09-30 00:03:14 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mod_page = require('./schemas/postpage.js')
 
 const connect = require('./schemas');
 connect();
@@ -21,36 +22,43 @@ app.use((req, res, next) => {
 
 const postPageRouter = require('./routers/postpage');
 app.use('/api', postPageRouter);
+
 //api 해당되는 파일들을  postpage에 저장 for api Server
 
 //Adding to install ejs
 //purpose: Recall file from ejs in webpage
-
+//ejs 템플릿 엔진을 위해 필요한 조건
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 
 
-//전체 게시글 목록 조회 페이지
+//views/main_page.ejs로 이동
+app.get('/', (req, res) => {
+    res.render('main_page')
+});
+//views/post_page.ejs 로 이동
 app.get('/list', (req, res) => {
     res.render('post_page')
 });
-//게시글 작성 페이지
+//views/write_page.ejs 로 이동 
 app.get('/writedetail', (req, res) => {
     res.render('write_page')
 });
-//게시글 조회 페이지
-app.get('/inquire', (req, res) => {
+//views/inquire_page.ejs 로 이동 
+app.get('/inquire', (req, res) => { //data received from querystring
     res.render('inquire_page')
 });
-//게시글 수정 페이지
+//views/fix_page.ejs 로 이동 
 app.get('/modify', (req, res) => {
+    //유일한 object 값을 가진 데이터를 찾는 작업// mongodb=전체(지구 같은 역할) //sept 29, 2020, 17:30pm done by here
+    //let selectone = req.query
+    //const mod_item = await mod_page.findOne({selected:selectone});
+   
+    //렌더링하는 작업
     res.render('fix_page')
 });
-//게시글 삭제 페이지
-app.get('/remove', (req, res) => {
-    res.render('delete_page')
-});
+
 
 //서버가동
 app.listen(port, () => {
