a53d91b4a8135a5ad615c96b2eedfe7f2b52c8ff
Sung Hyun Hong (Jenny) <57204918+Jenny9197@users.noreply.github.com>
2021-10-07 04:13:19 +0900

    Add files via upload

---
+++ public/static/callFunc.js
@@ -0,0 +1,31 @@
+//로그인 했을 경우, 로그인페이지로 이동
+//로그인 하지 않을 경우, error창 뜨게 함
+function callFunc() {
+    $.ajax({
+        type: "GET",
+        url: "api/checkSign",
+        success: function (response) {
+            
+        },
+        error: function (error){
+            alert(error.responseJSON.errorMsg);
+            window.location.href="/signIn";
+        },
+    });
+}
+
+
+//로그인 한 후, 목록 조회 페이지로 이동
+function uncallFunc() {
+    $.ajax({
+        type: "GET",
+        url: "api/checkSign",
+        success: function (response) {
+            alert(response['success']);
+            window.location.href="/list";
+        },
+        error: function (error){
+           
+        },
+    });
+}
